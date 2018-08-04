12b272fbb3f67da77c9e833889faa7fd55df69ee
amyadzuki <31188054+amyadzuki@users.noreply.github.com>
2018-08-04 07:36:33 +0000

    Bugfixes

--- dirs/dirs.go
+++ dirs/dirs.go
@@ -1,6 +1,7 @@
 package dirs
 
 import (
+	"errors"
 	"os"
 	"path/filepath"
 
@@ -8,21 +9,17 @@ import (
 )
 
 type Dirs struct {
-	vendor, application string
+	vendor, application, exedir string
 
-	sExecutableDirectory string
-	sSystemCache         string
-	sSystemConfig        string
-	sSystemData          string
-	sUserCache           string
-	sUserConfig          string
-	sUserData            string
-	sUserDesktop         string
-	sUserDocuments       string
-	sUserDownloads       string
-	sUserHome            string
-	sUserPictures        string
-	sUserScreenshots     string
+	sCache       string
+	sConfig      string
+	sData        string
+	sDesktop     string
+	sDocuments   string
+	sDownloads   string
+	sHome        string
+	sPictures    string
+	sScreenshots string
 }
 
 func New(vendor, application string, onFail ...onfail.OnFail) *Dirs {
@@ -33,8 +30,8 @@ func (d *Dirs) Application() string {
 	return d.application
 }
 
-func (d *Dirs) ExecutableDirectory() string {
-	return sExecutableDirectory
+func (d *Dirs) ExeDir() string {
+	return d.exedir
 }
 
 func (d *Dirs) Init(vendor, application string, onFail ...onfail.OnFail) *Dirs {
@@ -52,52 +49,40 @@ func (d *Dirs) Init(vendor, application string, onFail ...onfail.OnFail) *Dirs {
 	return d
 }
 
-func (d *Dirs) SystemCache() string {
-	return sSystemCache
+func (d *Dirs) Cache() string {
+	return d.sCache
 }
 
-func (d *Dirs) SystemConfig() string {
-	return sSystemConfig
+func (d *Dirs) Config() string {
+	return d.sConfig
 }
 
-func (d *Dirs) SystemData() string {
-	return sSystemData
+func (d *Dirs) Data() string {
+	return d.sData
 }
 
-func (d *Dirs) UserCache() string {
-	return sUserCache
+func (d *Dirs) Desktop() string {
+	return d.sDesktop
 }
 
-func (d *Dirs) UserConfig() string {
-	return sUserConfig
+func (d *Dirs) Documents() string {
+	return d.sDocuments
 }
 
-func (d *Dirs) UserData() string {
-	return sUserData
+func (d *Dirs) Downloads() string {
+	return d.sDownloads
 }
 
-func (d *Dirs) UserDesktop() string {
-	return sUserDesktop
+func (d *Dirs) Home() string {
+	return d.sHome
 }
 
-func (d *Dirs) UserDocuments() string {
-	return sUserDocuments
+func (d *Dirs) Pictures() string {
+	return d.sPictures
 }
 
-func (d *Dirs) UserDownloads() string {
-	return sUserDownloads
-}
-
-func (d *Dirs) UserHome() string {
-	return sUserHome
-}
-
-func (d *Dirs) UserPictures() string {
-	return sUserPictures
-}
-
-func (d *Dirs) UserScreenshots() string {
-	return sUserScreenshots
+func (d *Dirs) Screenshots() string {
+	return d.sScreenshots
 }
 
 func (d *Dirs) Vendor() string {
@@ -105,5 +90,5 @@ func (d *Dirs) Vendor() string {
 }
 
 var initDirs = func(d *Dirs, vendor, application string) {
-	return
+	panic(errors.New("OS not supported"))
 }
