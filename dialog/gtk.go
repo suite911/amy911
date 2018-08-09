4b59ad3b4a02cff383bbff506913c7838c5dfdf3
Amy <amyadzuki@gmail.com>
2018-08-09 08:22:27 +0000

    Update gtk.go

--- dialog/gtk.go
+++ dialog/gtk.go
@@ -33,8 +33,8 @@ func (f GtkFrame) NewButtonGroup(out *int8, g *ButtonGroup) {
 		b := gtk.NewButtonWithLabel(def.Label)
 		b.Clicked(func(ctx *glib.CallbackContext) {
 			if out != nil {
-				fmt.Printf("> %T: %v // %v\n", ctx.Data, def)
-//				*out = ctx.Data
+				fmt.Printf("> %T: %v // %v\n", ctx.Data(), ctx.Data(), def)
+//				*out = ctx.Data()
 			}
 			f.GetTopLevel().Destroy()
 		}, def.Result)
