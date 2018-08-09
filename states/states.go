eda5016c37a4c8d225a5dd1bb9cb6674aa20a406
Amy <amyadzuki@gmail.com>
2018-08-09 12:38:55 +0000

    Moving to amy911 organization...

--- states/states.go
+++ states/states.go
@@ -6,8 +6,8 @@ import (
 	"log"
 	"time"
 
-	"github.com/amyadzuki/amygolib/onfail"
-	"github.com/amyadzuki/amygolib/str"
+	"github.com/amy911/amy911/onfail"
+	"github.com/amy911/amy911/str"
 )
 
 var Debug, Note, Trace *log.Logger = nil, nil, nil
