15bea876e76c8147b8f7b97d6523317f3873f385
Amy <amyadzuki@gmail.com>
2018-08-09 08:46:23 +0000

    Bugfix

--- dialog/gtk.go
+++ dialog/gtk.go
@@ -97,7 +97,7 @@ func (w GtkWindow) NewFrame(title string) Frame {
 }
 
 func (w GtkWindow) Show(width, height int) {
-	w.SetSizeRequest(width, height)
-	w.ShowAll()
+	w.Window.SetSizeRequest(width, height)
+	w.Window.ShowAll()
 	gtk.Main()
 }
