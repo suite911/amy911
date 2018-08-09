557ea480a95b294907b5247b09271c36d99b831d
Amy <amyadzuki@gmail.com>
2018-08-09 16:12:34 +0000

    Create unix.go

---
+++ security/unix.go
@@ -0,0 +1,23 @@
+// +build freebsd linux netbsd openbsd solaris
+
+package security
+
+import (
+	"os"
+
+	"golang.org/x/sys/unix"
+
+	"github.com/amy911/amy911/onfail"
+)
+
+func chroot(path string, onFail onfail.OnFail) error {
+	if err := unix.Chroot(path); err != nil {
+		onfail.Fail(err, path, onfail.Panic, onFail)
+		return err
+	}
+	if err := os.Chdir("/"); err != nil {
+		onfail.Fail(err, path, onfail.Panic, onFail)
+		return err
+	}
+	return nil
+}
