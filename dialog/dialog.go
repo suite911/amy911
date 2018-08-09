c4195b6b3bc7c173814b5a90a4091a3d8d75b5f9
Amy <amyadzuki@gmail.com>
2018-08-09 02:50:23 +0000

    Update dialog.go

--- dialog/dialog.go
+++ dialog/dialog.go
@@ -1,7 +1,25 @@
 package dialog
 
+import (
+	"github.com/amyadzuki/amygolib/onfail"
+	"github.com/amyadzuki/amygolib/str"
+)
+
 var Library interface{}
 
 type Dialog struct {
 	Embed, UserData interface{}
+}
+
+func New(type string, onFail ...onfail.OnFail) *Dialog {
+	return new(Dialog).Init(type, onFail...)
+}
+
+func (d *Dialog) Init(type string, onFail ...onfail.OnFail) *Dialog {
+	switch simp := str.Simp(type); simp {
+	// case "login":
+	default:
+		onfail.Fail("Unknown Dialog type \""+simp+"\"", d, onfail.Panic, onFail...)
+	}
+	return d
 }
