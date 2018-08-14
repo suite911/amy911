2620f1ef48939ad7f39862214b75f88386308cfe
Amy <amyadzuki@gmail.com>
2018-08-14 16:09:16 +0000

    Update fma.go

--- maths/fma.go
+++ maths/fma.go
@@ -4,6 +4,6 @@ func Fma(base, mul, add float64) float64 {
 	return base * mul + add
 }
 
-func Fma32(base, mul, add float32) float64 {
+func Fma32(base, mul, add float32) float32 {
 	return float32(Fma(float64(base), float64(mul), float64(add)))
 }
