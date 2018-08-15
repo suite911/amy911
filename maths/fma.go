2d56e31418769a875f727c61ebf0ac288bbbda18
Amy <amyadzuki@gmail.com>
2018-08-15 13:29:01 +0000

    Delete fma.go

--- maths/fma.go
+++
@@ -1,9 +0,0 @@
-package maths
-
-func Fma(base, mul, add float64) float64 {
-	return base * mul + add
-}
-
-func Fma32(base, mul, add float32) float32 {
-	return float32(Fma(float64(base), float64(mul), float64(add)))
-}
